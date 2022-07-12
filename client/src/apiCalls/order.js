ea402a1c993ef8574f209755edd36bc7d9249451
abhijhacodes <abhi.jha.cs@gmail.com>
2022-07-13 00:29:44 +0530

    checkout image

--- client/src/apiCalls/order.js
+++ client/src/apiCalls/order.js
@@ -1,1 +1,17 @@
-// todo: create order
+import API from "./index";
+
+export const createOrder = (userId, token, orderData) => {
+  return fetch(`${API}/order/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ order: orderData }),
+  })
+    .then((reponse) => {
+      return reponse.json();
+    })
+    .catch((err) => console.log(err));
+};
